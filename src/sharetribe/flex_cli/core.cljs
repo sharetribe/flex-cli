a48b955d7870ac97e84a7721adc776dbedffddef
Mikko Koski <mikkokos@gmail.com>
2019-05-22 10:25:24 +0300

    Read command-line args after code hot reloading

--- src/sharetribe/flex_cli/core.cljs
+++ src/sharetribe/flex_cli/core.cljs
@@ -1,5 +1,31 @@
-(ns sharetribe.flex-cli.core)
+(ns sharetribe.flex-cli.core
+  (:require [readline]
+            [clojure.string :as str]))
 
-(defn main [& cli-args]
+(defn main
+  "Main entrypoint for the CLI"
+  [& cli-args]
   (println "Welcome to Flex CLI")
-  (println "CLI argssss: " cli-args))
+  (println "CLI args: " cli-args))
+
+(defonce waiting-answer? (atom false))
+
+(defn create-readline! []
+  (doto (.createInterface readline #js {:output js/process.stdout
+                                        :input js/process.stdin})
+    (.setPrompt "flex> ")
+    (.on "line" #(when @waiting-answer?
+                   (let [cli-args (str/split % " ")]
+                     (apply main cli-args)
+                     (reset! waiting-answer? false))))))
+
+(defonce rl (create-readline!))
+
+(defn ^:dev/after-load after-load
+  "Callback function that is run after code hot loading. Prompts
+  command-line args and will call the main function with given args."
+  []
+  (println "")
+  (println (str "[" (.toISOString (js/Date.)) "] Code reloaded"))
+  (.prompt rl)
+  (reset! waiting-answer? true))
