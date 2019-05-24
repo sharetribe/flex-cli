47fb1b573b01d9121f1dfd82e5931a33b53b0a0b
Mikko Koski <mikkokos@gmail.com>
2019-05-24 13:03:17 +0300

    Add login logout

--- src/sharetribe/flex_cli/commands/auth.cljs
+++ src/sharetribe/flex_cli/commands/auth.cljs
@@ -1,9 +1,37 @@
-(ns sharetribe.flex-cli.commands.auth)
+(ns sharetribe.flex-cli.commands.auth
+  (:require [sharetribe.flex-cli.credential-store :as credential-store])
+  )
 
 (defn login [opts]
-  (println "TODO")
-  (println "login command, options:" opts))
+  ;; TODO Build generic parameter validation
+  (assert (:api-key opts) "Missing mandatory option: api-key")
+  (assert (:marketplace opts) "Missing mandatory option: marketplace")
+
+  ;; TODO Validate API key (by doing a request to API)?
+
+  (credential-store/set-api-key
+   ;; TODO Coerse parameters
+   (keyword (:marketplace opts))
+   (:api-key opts)
+   {:on-success #(println "API stored.")
+    :on-error #(println "Failed to store the API key. This may happen
+    if you are using an environment where a credential manager is not
+    available, e.g. a headless environment without desktop
+    capabilities.") ;; TODO Add note about possiblity to use API_KEY environment variable, once it's implemented.
+    }))
 
 (defn logout [opts]
-  (println "TODO")
-  (println "logout command, options:" opts))
+  (credential-store/delete-api-key
+   ;; TODO Coerse parameters
+   (keyword (:marketplace opts))
+   {:on-success #(println "API removed.")
+    :on-error #(println "Failed to store the API key. This may happen
+    if you are using an environment where a credential manager is not
+    available, e.g. a headless environment without desktop
+    capabilities.") ;; TODO Add note about possiblity to use API_KEY environment variable, once it's implemented.
+    })
+  )
+
+(comment
+  (sharetribe.flex-cli.core/main-dev-str "login --api-key=asdfasdfasdf --marketplace=bike-soil")
+  (sharetribe.flex-cli.core/main-dev-str "logout --marketplace=bike-soil"))
