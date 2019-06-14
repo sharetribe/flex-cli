6f7e61701335cdd8553f957427fbf0f0ab5eb2cb
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2019-06-14 13:44:26 +0300

    Tune error messages a bit

--- src/sharetribe/tempelhof/process_validation.cljs
+++ src/sharetribe/tempelhof/process_validation.cljs
@@ -4,16 +4,20 @@
             [sharetribe.tempelhof.spec]
             [sharetribe.flex-cli.exception :as exception]))
 
-(expound/defmsg :tx-process.transition/name "Transition name should be a keyword")
-(expound/defmsg :tx-process.transition/to "Transition to should be a keyword")
-(expound/defmsg :tx-process.transition/from "Transition from should be a keyword")
+(expound/defmsg :tx-process.transition/name ":name should be a keyword that uniquely names the transition in the process. Example: :transition/request")
+(expound/defmsg :tx-process.transition/to ":to should be a keyword that references a state in the process. Example: :state/requested")
+(expound/defmsg :tx-process.transition/from ":from should be a keyword that references a state in the process. It can be left out if this is an initial transition. Example: :state/requested")
+
+(expound/defmsg :tx-process.transition/action "Transition's action should be defined as an action description map. Example: {:name :action/action-name :config {:config-property \"configuration value\"}}")
+
 
 (defmethod exception/format-exception :tx-process/invalid-process [_ _ {:keys [tx-process spec]}]
   (let [printer (expound/custom-printer {:print-specs? false
                                          :theme :figwheel-theme})
-        explain-data (s/explain-data spec tx-process)]
-    (with-out-str
-      (printer explain-data))))
+        explain-data (s/explain-data spec tx-process)
+        validation-errors (with-out-str (printer explain-data))]
+    (str "The process description is not valid\n\n"
+         validation-errors)))
 
 (defn validate!
   "Validates a v3 process map. Throws an exception if the process is
