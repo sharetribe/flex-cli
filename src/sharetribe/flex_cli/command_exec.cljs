b1908b0a5e7173abc99fa80c5dd08049eb19fb6c
Mikko Koski <mikkokos@gmail.com>
2019-08-14 10:09:53 +0300

    Add namespace async-util
    Move ensure-chan function there

--- src/sharetribe/flex_cli/command_exec.cljs
+++ src/sharetribe/flex_cli/command_exec.cljs
@@ -1,26 +1,10 @@
 (ns sharetribe.flex-cli.command-exec
   "Namespace for executing a command after the CLI arguments are parsed"
   (:require [clojure.core.async :as async :refer [go <!]]
-            [clojure.core.async.impl.protocols :as async.protocols]
             [sharetribe.flex-cli.credential-store :as credential-store]
             [sharetribe.flex-cli.api.client :as api-client]
-            [sharetribe.flex-cli.exception :as exception]))
-
-(defn- read-port?
-  "Checks if x is read port. There's no predicate for `chan?`, thus this
-  is the way to do it. See:
-  https://clojure.atlassian.net/browse/ASYNC-74"
-  [x]
-  (satisfies? async.protocols/ReadPort x))
-
-(defn- ensure-chan
-  "Takes x and ensures it's a channel. If not, it will wrap the value in
-  a go block (which returns values). Helps to normalize operations
-  with may be async or sync."
-  [x]
-  (if (read-port? x)
-    x
-    (go x)))
+            [sharetribe.flex-cli.exception :as exception]
+            [sharetribe.flex-cli.async-util :refer [ensure-chan]]))
 
 ;; Public
 
