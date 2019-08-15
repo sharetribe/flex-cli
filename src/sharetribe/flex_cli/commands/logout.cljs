d14622e3c506bc373bc8c38d6b8015b646d659fb
Mikko Koski <mikkokos@gmail.com>
2019-08-15 09:56:55 +0300

    Add output to logout command

--- src/sharetribe/flex_cli/commands/logout.cljs
+++ src/sharetribe/flex_cli/commands/logout.cljs
@@ -1,5 +1,11 @@
 (ns sharetribe.flex-cli.commands.logout
-  (:require [sharetribe.flex-cli.credential-store :as credential-store]))
+  (:require [clojure.core.async :as async :refer [go <!]]
+            [sharetribe.flex-cli.credential-store :as credential-store]
+            [sharetribe.flex-cli.io-util :as io-util]))
 
 (defn logout [opts _]
-  (credential-store/delete-api-key))
+  (go
+    (<! (credential-store/delete-api-key))
+
+    (io-util/ppd
+     [:span "Successfully logged out."])))
