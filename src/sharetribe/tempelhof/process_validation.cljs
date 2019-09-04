8a35aac6d414bb12787eea734b74fe80a25d0326
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2019-09-04 15:36:11 +0300

    Drop expound

--- src/sharetribe/tempelhof/process_validation.cljs
+++ src/sharetribe/tempelhof/process_validation.cljs
@@ -2,72 +2,13 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.set :as set]
             [clojure.string :as str]
-            [expound.alpha :as expound]
             [phrase.alpha :as phrase :refer [defphraser]]
             [chalk]
             [sharetribe.tempelhof.spec]
             [sharetribe.flex-cli.exception :as exception]
             [sharetribe.tempelhof.spec :as tempelhof.spec]))
 
-;; Message formatting for specific key specs
-;;
-
-(expound/defmsg :tempelhof/tx-process "Process should be defined as a map. Example: {:format :v3 :transitions [ ... ]}")
-
-;; :tx-process/format - default message ok
-
-(expound/defmsg :tx-process/transitions ":transitions should be collection of transitions with unique names. Example: :transitions [ ... ]")
-(expound/defmsg :tx-process/notifications ":notifications should be collection of notifications with unique names. Example: :notifications [ ... ]")
-
-;; Transitions
-
-(expound/defmsg :tx-process/transition "Transition should be defined as a map. Example: {:name :transition/my-transition ... }")
-(expound/defmsg :tx-process.transition/name ":name should be a keyword that uniquely names the transition in the process. Example: :transition/request")
-(expound/defmsg :tx-process.transition/to ":to should be a keyword that references a state in the process. Example: :state/requested")
-(expound/defmsg :tx-process.transition/from ":from should be a keyword that references a state in the process. It can be left out if this is an initial transition. Example: :state/requested")
-(expound/defmsg :tx-process.transition/actions ":actions should be collection of actions. Example: :actions [ ... ]")
-
-;; :tx-process.transition/actor - ok as default
-;; :tx-process.transition/at - default is probably better that anything generic
-
-;; Actions
-
-(expound/defmsg :tx-process.transition/action "Transition's action should be defined as an action description map. Example: {:name :action/action-name :config {:config-property \"configuration value\"}}")
-
-;; :tx-process.action/name - default message ok
-
-(expound/defmsg :tx-process.action/config ":config should be a map with configuration properties specific to the action. Example: :config {:config-property \"configuration value\"}")
-
-;; Notifications
-
-(expound/defmsg :tx-process/notification "Notification should be defined as a map. Example: {:name :notification/my-notification ... }")
-(expound/defmsg :tx-process.notification/name ":name should be a keyword that uniquely names the notification in the process. Example: :notification/my-notification-name")
-(expound/defmsg :tx-process.notification/on ":on should be keyword that references a state in the process. Example :state/requested")
-
-;; :tx-process.notification/to - default message ok
-
-(expound/defmsg :tx-process.notification/template ":template should be a keyword that matches the corresponding email template name. Example: :new-booking-request")
-
-;; :tx-process.notification/at - default is probably better that anything generic
-
-(expound/defmsg :tx-process.notification/on-transition-name "Notification :on should point to an existing transition name.")
-
-
-#_(defmethod exception/format-exception :tx-process/invalid-process [_ _ {:keys [tx-process spec]}]
-  (let [printer (expound/custom-printer {:print-specs? false
-                                         :theme :figwheel-theme})
-        explain-data (s/explain-data spec tx-process)
-        validation-errors (with-out-str (printer explain-data))]
-    (str "The process description is not valid\n\n"
-         validation-errors)))
-
-(defn- expound-error-str [tx-process spec]
-  (let [printer (expound/custom-printer {:print-specs? false
-                                         :theme :none})
-        explain-data (s/explain-data spec tx-process)
-        validation-errors (with-out-str (printer explain-data))]
-    validation-errors))
-
+;; TODO Error messages for invalid .edn files?
 
 (defn- location
   "Pull out location info for the given form / process description
