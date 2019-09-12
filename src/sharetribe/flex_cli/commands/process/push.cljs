a68189be3ebde3b5b2673a871ec359caa87638ad
Kimmo Puputti <git@kimmo.kpuputti.fi>
2019-09-12 14:49:21 +0300

    Extract process exception utils

--- src/sharetribe/flex_cli/commands/process/push.cljs
+++ src/sharetribe/flex_cli/commands/process/push.cljs
@@ -6,56 +6,19 @@
             [sharetribe.flex-cli.io-util :as io-util]
             [sharetribe.flex-cli.api.client :as api.client :refer [do-post]]
             [sharetribe.flex-cli.exception :as exception]
-            [sharetribe.tempelhof.tx-process :as tx-process]))
-
-(def error-arrow (.bold.red chalk "\u203A"))
-
-(defn- error-report
-  "Given an error description map, format it as an error report string
-  (a multi line string)."
-  [total index error]
-  (let [{:keys [loc msg]} error
-        {:keys [row col]} loc
-        header (if loc
-                 (str (inc index) "/" total
-                      " [at line " row ", column " col "]"
-                      ":\n")
-                 (str (inc index) "/" total
-                      ":\n"))]
-    (str "\n" error-arrow " " header msg "\n")))
-
-(defn template-error-report [total index error]
-  (let [{:keys [template-name reason evidence line column template-part]} error]
-    (error-report
-     total
-     index
-     {:msg (str "Error in " (.bold chalk (name template-name))
-                " template " (name template-part)
-                ". Reason: " reason
-                "\n\n" evidence)
-      :loc {:row line
-            :col column}})))
-
-(defn format-invalid-templates-error [data]
-  (let [errors (-> (api.client/api-error data) :details :render-errors)
-        total-errors (count errors)]
-    (concat
-     [:span
-      "The process contains invalid email templates. Found "
-      (str total-errors) " invalid " (if (= 1 total-errors) "template" "templates") "."
-      :line]
-     (map-indexed (partial template-error-report total-errors) errors))))
+            [sharetribe.tempelhof.tx-process :as tx-process]
+            [sharetribe.flex-cli.commands.process.exception-util :as process.exception-util]))
 
 (defmethod exception/format-exception :process.push/api-call-failed [_ _ data]
   (case (:code (api.client/api-error data))
-    :invalid-templates (format-invalid-templates-error data)
+    :invalid-templates (process.exception-util/format-invalid-templates-error data)
     (api.client/default-error-format data)))
 
 (defmethod exception/format-exception :process.push/missing-templates [_ _ {:keys [notifications]}]
   [:span
    (map (fn [{:keys [name template]}]
-          [:span (.bold.red chalk "Error: ")
-           "template " (.bold chalk (clojure.core/name template))
+          [:span process.exception-util/error-arrow
+           " Template " (.bold chalk (clojure.core/name template))
            " not found for notification " (.bold chalk (clojure.core/name name))
            :line])
         notifications)])
