1ab96f5ae32d66c05003dc6d49361bdffe5ceb95
Kimmo Puputti <git@kimmo.kpuputti.fi>
2019-11-21 14:36:44 +0200

    Leave the server on for other API errors as well

--- src/sharetribe/flex_cli/commands/notifications/preview.cljs
+++ src/sharetribe/flex_cli/commands/notifications/preview.cljs
@@ -61,7 +61,8 @@
   (str (.bold chalk "Template: ") (name (:name template))
        (.bold chalk "\nSubject: ") subject
        (.bold chalk "\nText:\n") text
-       "\n---"))
+       "\n---\n"
+       "See " preview-server-url " for the HTML preview. Refresh the browser to preview again. Type <Ctrl>+C to quit."))
 
 (defn fetch-preview!
   "Fetch a template preview"
@@ -85,42 +86,47 @@
                 :error e}))))
     c))
 
-(defn create-preview-request-handler [opts done-chan]
-  (let [handle-invalid-template (fn [^js res data]
-                                  (let [msg "Invalid template. See terminal output for details."
-                                        body-style "font-size: 32px; color: red"
-                                        error-html (str "<html><body style=\"" body-style "\"><p>" msg "</p></body></html>")]
-                                    (io-util/ppd-err
-                                     (process-util/format-invalid-template-error data))
-                                    (io-util/ppd-err "Fix the errors above and refresh the preview URL in the browser to continue.")
-                                    (set! (.-statusCode res) 500)
-                                    (doto res
-                                      (.setHeader "Content-Type" "text/html")
-                                      (.end error-html))))
-        handle-error (fn [res error]
-                       (let [data (exception/data error)]
-                         (case (:code (api.client/api-error data))
-                           :invalid-template (handle-invalid-template res data)
-                           (do (io-util/ppd-err (api.client/default-error-format data))
-                               (close! done-chan)))))
-        handle-success (fn [^js res template]
-                         (println (format-template template))
-                         (set! (.-statusCode res) 200)
-                         (doto res
-                           (.setHeader "Content-Type" "text/html")
-                           (.write (inject-title template))
-                           (.end)))
-        handle-not-found (fn [^js res]
-                           (set! (.-statusCode res) 404)
-                           (.end res))]
-    (fn [^js req ^js res]
-      (if (= "/" (.-url req))
-        (go
-          (let [{:keys [template error]} (<! (fetch-preview! opts))]
-            (if error
-              (handle-error res error)
-              (handle-success res template))))
-        (handle-not-found res)))))
+(defn handle-error [^js res error]
+  (let [data (exception/data error)
+        {:keys [out msg]} (case (:code (api.client/api-error data))
+                            :invalid-template
+                            {:msg "Invalid template. See terminal output for details."
+                             :out (process-util/format-invalid-template-error data)}
+
+                            {:msg "Error in API call. See terminal output for details."
+                             :out (api.client/default-error-format data)})
+        body-style "font-size: 32px; color: red"
+        error-html (str "<html><body style=\"" body-style "\"><p>" msg "</p></body></html>")]
+    (io-util/ppd-err out)
+    (io-util/ppd-err (str "Fix the errors above and refresh "
+                          preview-server-url
+                          " in the browser to continue. Type <Ctrl>+C to quit."))
+    (set! (.-statusCode res) 400)
+    (doto res
+      (.setHeader "Content-Type" "text/html")
+      (.end error-html))))
+
+(defn handle-success [^js res template]
+  (println (format-template template))
+  (set! (.-statusCode res) 200)
+  (doto res
+    (.setHeader "Content-Type" "text/html")
+    (.write (inject-title template))
+    (.end)))
+
+(defn handle-not-found [^js res]
+  (set! (.-statusCode res) 404)
+  (.end res))
+
+(defn create-preview-request-handler [opts]
+  (fn [req res]
+    (if (= "/" (.-url req))
+      (go
+        (let [{:keys [template error]} (<! (fetch-preview! opts))]
+          (if error
+            (handle-error res error)
+            (handle-success res template))))
+      (handle-not-found res))))
 
 (defn preview [params ctx]
   (let [{:keys [api-client marketplace]} ctx
@@ -133,12 +139,11 @@
     (.on js/process "SIGINT" (fn [_]
                                (close! done-chan)))
     (go
-      (<! (start-preview-server! (create-preview-request-handler opts done-chan)))
+      (<! (start-preview-server! (create-preview-request-handler opts)))
 
-      (println "Opening preview at" preview-server-url)
+      (println "Opening preview HTML in a browser.")
       (open preview-server-url)
 
-      (println "Type <Ctrl>+C to quit")
       (<! done-chan)
       (println "Preview server closed."))))
 
