786f0c3cff9080b2667c7dd01baf2a03387a1ab0
Kimmo Puputti <git@kimmo.kpuputti.fi>
2019-11-21 14:05:00 +0200

    Refactor code based on review
    - Only call API on preview server request
    - Change port
    - Print template errors instead of quitting
    - Wait for SIGINT to quit

--- src/sharetribe/flex_cli/commands/notifications/preview.cljs
+++ src/sharetribe/flex_cli/commands/notifications/preview.cljs
@@ -1,6 +1,6 @@
 (ns sharetribe.flex-cli.commands.notifications.preview
   (:require [clojure.string :as str]
-            [clojure.core.async :as async :refer [go <! >! chan put!]]
+            [clojure.core.async :as async :refer [go <! >! chan put! close!]]
             [sharetribe.flex-cli.async-util :refer [<? go-try]]
             [sharetribe.flex-cli.io-util :as io-util]
             [sharetribe.flex-cli.process-util :as process-util]
@@ -26,17 +26,30 @@
                   :desc "path to an email rendering context JSON file"
                   :required "CONTEXT_FILE_PATH"}]})
 
-(def port 3000)
+;; Some random port where the preview server is opened. Should not
+;; clash with other common Flex tooling.
+(def port 3535)
+(def preview-server-url (str "http://localhost:" port))
+
 (def preview-server (atom nil))
-(def preview-template (atom nil))
 
-(defn bold [str]
-  (.bold chalk str))
+(defn start-preview-server!
+  "Start a new preview server
 
-(defmethod exception/format-exception :notifications.preview/api-call-failed [_ _ data]
-  (case (:code (api.client/api-error data))
-    :invalid-template (process-util/format-invalid-template-error data)
-    (api.client/default-error-format data)))
+  Closes and waits for the existing channel before starting listening
+  for requests with the new one. Returns a channel that closes when
+  the new server is listening for requests."
+  [request-handler]
+  (let [close-chan (chan)
+        listening-chan (chan)
+        server (.createServer http request-handler)]
+    (go
+      (when-let [s @preview-server]
+        (reset! preview-server server)
+        (.close s #(close! close-chan))
+        (<! close-chan))
+      (.listen server port #(close! listening-chan))
+      (<! listening-chan))))
 
 (defn inject-title
   "Inject subject as a <title> tag to the HTML"
@@ -45,69 +58,89 @@
     (str/replace html #"^<html>" (str "<html>" head))))
 
 (defn format-template [{:keys [subject text] :as template}]
-  (str (bold "Template: ") (name (:name template))
-       (bold "\nSubject: ") subject
-       (bold "\nText:\n") text
+  (str (.bold chalk "Template: ") (name (:name template))
+       (.bold chalk "\nSubject: ") subject
+       (.bold chalk "\nText:\n") text
        "\n---"))
 
-(defn update-preview!
-  "Update the template preview
-
-  - Fetch new preview
-  - Update response to the preview-template atom
-  - Print out the text template"
+(defn fetch-preview!
+  "Fetch a template preview"
   [opts]
   (let [{:keys [api-client marketplace template context]} opts
         query-params {:marketplace marketplace}
         body-params (cond-> {:template (io-util/read-template template)}
-                      context (assoc :template-context (io-util/load-file context)))]
+                      context (assoc :template-context (io-util/load-file context)))
+        c (chan)]
     (println "Fetching a new preview...")
     (go-try
-     (let [res (try
-                 (<? (do-post api-client
+     (try
+       (let [res (<? (do-post api-client
                               "/notifications/preview"
                               query-params
-                              body-params))
-                 (catch js/Error e
-                   (throw
-                    (api.client/retype-ex e :notifications.preview/api-call-failed))))
-           tmpl (:data res)]
-       (println (format-template tmpl))
-       (reset! preview-template tmpl)))))
+                              body-params))]
+         (>! c {:template (:data res)
+                :error nil}))
+       (catch js/Error e
+         (>! c {:template nil
+                :error e}))))
+    c))
 
-(defn create-request-handler [opts]
-  (let [first-request (atom true)]
+(defn create-preview-request-handler [opts done-chan]
+  (let [handle-invalid-template (fn [^js res data]
+                                  (let [msg "Invalid template. See terminal output for details."
+                                        body-style "font-size: 32px; color: red"
+                                        error-html (str "<html><body style=\"" body-style "\"><p>" msg "</p></body></html>")]
+                                    (io-util/ppd-err
+                                     (process-util/format-invalid-template-error data))
+                                    (io-util/ppd-err "Fix the errors above and refresh the preview URL in the browser to continue.")
+                                    (set! (.-statusCode res) 500)
+                                    (doto res
+                                      (.setHeader "Content-Type" "text/html")
+                                      (.end error-html))))
+        handle-error (fn [res error]
+                       (let [data (exception/data error)]
+                         (case (:code (api.client/api-error data))
+                           :invalid-template (handle-invalid-template res data)
+                           (do (io-util/ppd-err (api.client/default-error-format data))
+                               (close! done-chan)))))
+        handle-success (fn [^js res template]
+                         (println (format-template template))
+                         (set! (.-statusCode res) 200)
+                         (doto res
+                           (.setHeader "Content-Type" "text/html")
+                           (.write (inject-title template))
+                           (.end)))
+        handle-not-found (fn [^js res]
+                           (set! (.-statusCode res) 404)
+                           (.end res))]
     (fn [^js req ^js res]
       (if (= "/" (.-url req))
         (go
-          (when-not @first-request
-            (<! (update-preview! opts)))
-          (reset! first-request false)
-          (set! (.-statusCode res) 200)
-          (doto res
-            (.setHeader "Content-Type" "text/html")
-            (.write (inject-title @preview-template))
-            (.end)))
-        (do (set! (.-statusCode res) 404)
-            (.end res))))))
+          (let [{:keys [template error]} (<! (fetch-preview! opts))]
+            (if error
+              (handle-error res error)
+              (handle-success res template))))
+        (handle-not-found res)))))
 
 (defn preview [params ctx]
   (let [{:keys [api-client marketplace]} ctx
         {:keys [template context]} params
+        done-chan (chan)
         opts {:api-client api-client
               :marketplace marketplace
               :template template
-              :context context}
-        server (.createServer http (create-request-handler opts))
-        url (str "http://localhost:" port)]
-    (when @preview-server
-      (.close @preview-server))
-    (reset! preview-server server)
+              :context context}]
+    (.on js/process "SIGINT" (fn [_]
+                               (close! done-chan)))
     (go
-      (<! (update-preview! opts))
-      (.listen server port #(println "Opening preview at" url))
-      (open url))
-    (chan)))
+      (<! (start-preview-server! (create-preview-request-handler opts done-chan)))
+
+      (println "Opening preview at" preview-server-url)
+      (open preview-server-url)
+
+      (println "Type <Ctrl>+C to quit")
+      (<! done-chan)
+      (println "Preview server closed."))))
 
 (comment
   (sharetribe.flex-cli.core/main-dev-str "notifications preview -m bike-soil --template test-process/templates/booking-request-accepted --context test-process/sample-context.json")
